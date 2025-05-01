55fd9426e2243e501d8413f3d6ba077ef3f14365
Fariha <alamfariha560@gmail.com>
2025-05-01 22:28:05 +0100

    21. MergedTwoSortedLists
    Topics: 
    - Linked List
    - Recursion
    
    Level: Easy

---
+++ Java/021MergedTwoSortedLists.java
@@ -0,0 +1,35 @@
+/**
+* You are given the heads of two sorted linked lists list1 and list2.
+* Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+* Return the head of the merged linked list.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+      
+        temp.next = l1 != null ? l1 : l2;
+        return head.next;
+    }
+}
