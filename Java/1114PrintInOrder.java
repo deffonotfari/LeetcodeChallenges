03cf838daf2b17bd2242360754b634efef85cf93
Fariha <alamfariha560@gmail.com>
2025-05-20 20:03:38 +0100

    1114. Print In Order.java
    Suppose we have a class:
    
    public class Foo {
      public void first() { print("first"); }
      public void second() { print("second"); }
      public void third() { print("third"); }
    }
    The same instance of Foo will be passed to three different threads. Thread A will call first(), thread B will call second(), and thread C will call third(). Design a mechanism and modify the program to ensure that second() is executed after first(), and third() is executed after second().
    
    Note:
    We do not know how the threads will be scheduled in the operating system, even though the numbers in the input seem to imply the ordering. The input format you see is mainly to ensure our tests' comprehensiveness.
    
    
    Constraints:
    - nums is a permutation of [1, 2, 3].
    
    Level: 
    Easy
    
    Topics:
    - Concurrency
    
    Date Completed: 20/05/2025

---
+++ Java/1114PrintInOrder.java
@@ -0,0 +1,23 @@
+class Foo {
+    private AtomicInteger firstDone = new AtomicInteger();
+    private AtomicInteger secondDone = new AtomicInteger();
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        printFirst.run();
+        firstDone.incrementAndGet();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        while (firstDone.get() != 1)
+            ;
+        
+        printSecond.run();
+        secondDone.incrementAndGet();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        while (secondDone.get() != 1)
+          ;
+        printThird.run();
+    }
+}
