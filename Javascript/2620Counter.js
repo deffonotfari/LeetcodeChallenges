a59f85d9369870aed4de84d15145b07ec5e22fb7
Fariha <alamfariha560@gmail.com>
2025-05-20 19:55:11 +0100

    2620. Counter
    Problem:
    Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
    
    Constraints:
    - -1000 <= n <= 1000
    - 0 <= calls.length <= 1000
    - calls[i] === "call"
    
    Level: Easy

---
+++ Javascript/2620Counter.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ *
+ * Given an integer n, return a counter function. 
+ * This counter function initially returns n and then returns 1 more 
+ * than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        let counter = n;
+        n++;
+        return counter;
+    };
+};
